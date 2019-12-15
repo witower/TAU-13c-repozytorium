41f59fd9fd9656a248f684ea73cef50083bac460
witower <s16125@pjwstk.edu.pl>
2019-12-15 15:24:27 +0100

    lab04 init

---
+++ jmeter/src/main/java/pl/edu/pjatk/tau/jmeter/JmeterApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.tau.jmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmeterApplication.class, args);
+    }
+
+}
---
+++ jmeter/src/test/java/pl/edu/pjatk/tau/jmeter/JmeterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.tau.jmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
