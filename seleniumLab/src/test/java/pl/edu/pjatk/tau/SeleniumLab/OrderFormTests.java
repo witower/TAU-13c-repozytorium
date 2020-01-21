acf8eea719222f6392ef8223b20503c24ba1e516
witower <s16125@pjwstk.edu.pl>
2020-01-21 23:10:19 +0100

    ScreenShotsOnFailure implemented, plus some refactoring

--- seleniumLab/src/test/java/pl/edu/pjatk/tau/SeleniumLab/OrderFormTests.java
+++ seleniumLab/src/test/java/pl/edu/pjatk/tau/SeleniumLab/OrderFormTests.java
@@ -1,4 +1,4 @@
-package pl.edu.pjatk.tau.SeleniumLab.orderForm;
+package pl.edu.pjatk.tau.SeleniumLab;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
@@ -9,25 +9,37 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class CompletedSuccessTest {
-    private WebDriver driver;
-    private String baseUrl;
+public class OrderFormTests {
+    private static WebDriver driver;
+    private  String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
+    @Rule
+    public ScreenShotOnFailure failure = new ScreenShotOnFailure(driver);
+
+    @BeforeClass
+    public static void setUpBc(){
+        driver = new ChromeDriver();
+    }
+
     @Before
     public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        baseUrl = "";
+        baseUrl = "https://roberts.pl/index.php?l=en&p=_katalog&i=_neutron_plus";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        driver.findElement(By.name("SubmitNext")).click();
+        try {
+            assertTrue(driver.getCurrentUrl().contains("p=_zamowienie&i=_neutron_plus"));
+            assertEquals("1", driver.findElement(By.name("step")).getAttribute("value"));
+        } catch (Exception e) {
+            verificationErrors.append(e.getMessage());
+        }
     }
 
     @Test
     public void testOrderFormSuccess() throws Exception {
-        driver.get("https://roberts.pl/index.php?l=en&p=_katalog&i=_neutron_plus");
-        driver.findElement(By.name("SubmitNext")).click();
-        assertTrue(driver.getCurrentUrl().matches("^https://roberts\\.pl/index\\.php[\\s\\S]l=en&p=_zamowienie&i=_neutron_plus$"));
-        assertEquals("1", driver.findElement(By.name("step")).getAttribute("value"));
+
         driver.findElement(By.name("krok1-qty-required")).click();
         driver.findElement(By.name("krok1-qty-required")).clear();
         driver.findElement(By.name("krok1-qty-required")).sendKeys("2");
@@ -35,12 +47,15 @@ public class CompletedSuccessTest {
         driver.findElement(By.name("krok1-info1")).clear();
         driver.findElement(By.name("krok1-info1")).sendKeys("Example comment");
         driver.findElement(By.cssSelector("[type='submit']")).click();
-        assertTrue(driver.getCurrentUrl().matches("^https://roberts\\.pl/index\\.php[\\s\\S]l=en&p=_zamowienie$"));
-        assertEquals("2", driver.findElement(By.name("step")).getAttribute("value"));
+        try {
+            assertEquals("2", driver.findElement(By.name("step")).getAttribute("value"));
+        } catch (Exception e) {
+            verificationErrors.append(e.getMessage());
+        }
+
         driver.findElement(By.xpath("(//input[@name='krok2-sposob_dostawy-required'])[3]")).click();
         driver.findElement(By.id("datepicker")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Links'])[2]/following::span[1]")).click();
-        driver.findElement(By.linkText("30")).click();
+        driver.findElement(By.cssSelector(".ui-datepicker-calendar [data-handler='selectDay']")).click();
         driver.findElement(By.xpath("(//input[@name='krok2-rodzaj_platnosci-required'])[2]")).click();
         driver.findElement(By.xpath("(//input[@name='krok2-dokument_zakupu-required'])[2]")).click();
         driver.findElement(By.name("krok2-imie-required")).click();
@@ -55,22 +70,21 @@ public class CompletedSuccessTest {
         driver.findElement(By.name("krok2-telefon-required")).clear();
         driver.findElement(By.name("krok2-telefon-required")).sendKeys("123456789");
         driver.findElement(By.name("SubmitNext")).click();
-        assertTrue(driver.getCurrentUrl().matches("^https://roberts\\.pl/index\\.php[\\s\\S]l=en&p=_zamowienie$"));
-        assertEquals("3", driver.findElement(By.name("step")).getAttribute("value"));
+        try {
+            assertEquals("3", driver.findElement(By.name("step")).getAttribute("value"));
+        } catch (Exception e) {
+            verificationErrors.append(e.getMessage());
+        }
+
         driver.findElement(By.xpath("(//input[@name='akceptuje_regulamin-required'])[2]")).click();
         driver.findElement(By.name("SubmitNext")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) fail("timeout");
-            try {
-                if ("4".equals(driver.findElement(By.name("step")).getAttribute("value"))) break;
-            } catch (Exception e) {
-                verificationErrors.append(e.getMessage());
-            }
-            Thread.sleep(1000);
+        try {
+            assertEquals("4", driver.findElement(By.name("step")).getAttribute("value"));
+        } catch (Exception e) {
+            verificationErrors.append(e.getMessage());
         }
 
         driver.findElement(By.linkText("Click here to go back to the page before starting inquiry.")).click();
-
         try {
             assertTrue(driver.getCurrentUrl().matches("^https://roberts\\.pl/index\\.php[\\s\\S]l=en&p=_katalog&i=_neutron_plus$"));
         } catch (Exception e) {
@@ -80,10 +94,17 @@ public class CompletedSuccessTest {
 
     @After
     public void tearDown() throws Exception {
-        driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
+        }
+    }
+
+    @AfterClass
+    public static void cleanUp(){
+        if (driver != null){
+            driver.close();
+            driver.quit();
         }
     }
 
